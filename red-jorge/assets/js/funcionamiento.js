6b9e5dd6f325cbd8c5f4ebe353a5d102abf7276a
Jul879n <arayacontrerasjulian@gmail.com>
2023-07-17 00:46:19 -0400

    calc
    faltan filtrar las calculadoras

--- red-jorge/assets/js/funcionamiento.js
+++ red-jorge/assets/js/funcionamiento.js
@@ -13,4 +13,21 @@ $(document).ready(function () {
     $(this).find("i").toggleClass("fa-sun fa-moon");
     $("body").toggleClass("tema-claro tema-oscuro");
   });
+  $("#contenido").on("submit", "#login-form", function(event) {
+    event.preventDefault(); // previene el comportamiento predeterminado del formulario
+    // obtiene los valores del formulario
+    var username = $('#email').val();
+    var password = $('#contrasena').val();
+    // verifica si los datos son correctos
+    if (username == 'jorgea.misolar@gmail.com' && password == 'Koke2001') {
+      // establece la sesión como iniciada
+      console.log("Sesión iniciada");
+      sessionStorage.setItem('isLoggedIn', true);
+      // carga la página si los datos son correctos
+    $("#contenido").load("calc.html");
+    } else {
+      // muestra una alerta si los datos son incorrectos
+      alert('Los datos ingresados son incorrectos. Por favor, inténtalo de nuevo.');
+    }
+  });
 });
